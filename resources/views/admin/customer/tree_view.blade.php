cb51334cb5589c144b722ec62b551535582f31b4
Snehasish Bhurisrestha <codofficial2023snehasish@gmail.com>
2024-12-07 12:55:33 +0530

    07-12-2024

--- resources/views/admin/customer/tree_view.blade.php
+++ resources/views/admin/customer/tree_view.blade.php
@@ -31,10 +31,31 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body table-responsive" style="/*display:flex;justify-content:center;*/">
-                                <div class="text-end">
+                                {{-- <div class="text-end">
                                     <button onclick="history.back()" class="btn btn-outline-success">
                                         <img src="{{ asset('dashboard_assets/images/back.png') }}" width="36px" height="26px" alt="">Back
                                     </button>
+                                </div> --}}
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <!-- Search Form -->
+                                    <div>
+                                        <form action="{{ route('report.business-report.tree') }}" method="get" class="d-flex" id="search-form">
+                                            <input type="search" id="search-query" class="form-control form-control-sm me-2" placeholder="Search by name or ID" name="query" aria-controls="datatable-buttons" minlength="3" autocomplete="off">
+                                            {{-- <input type="submit" class="btn btn-primary" value="Search"> --}}
+                                        </form>
+                                        
+                                        <!-- Suggestions Dropdown -->
+                                        <div id="suggestions" class="list-group position-absolute" style="display: none; z-index: 999;"></div>
+                                        
+                                    </div>
+
+                                    <!-- Back Button -->
+                                    <div>
+                                        <button onclick="history.back()" class="btn btn-outline-success">
+                                            <img src="{{ asset('dashboard_assets/images/back.png') }}" width="36px" height="26px" alt=""> Back
+                                        </button>
+                                    </div>
+                                
                                 </div>
                                 <div class="body genealogy-body genealogy-scroll">
                                     <div class="genealogy-tree">
@@ -70,6 +91,69 @@
                         }
                     });
                 }
+            </script>
+            <script>
+                $(document).ready(function() {
+                    $('#search-query').on('keyup', function() {
+                        var query = $(this).val();
+
+                        var suggestionList = $('#suggestions');
+                        // suggestionList.empty();
+                        suggestionList.html('');
+                
+                        if(query.length >= 3) {
+                            // Send AJAX request to fetch suggestions
+                            $.ajax({
+                                url: "{{ route('search-customers') }}",
+                                method: 'GET',
+                                data: { query: query },
+                                success: function(data) {
+                                    suggestionList.html('');
+                                    var suggestions = data.suggestions;
+                                    // var suggestionList = $('#suggestions');
+                                    // suggestionList.empty(); // Clear previous suggestions
+                
+                                    if(suggestions.length > 0) {
+                                        suggestionList.show(); // Show suggestions
+
+                                        var uniqueSuggestions = [];
+                                        var seenIds = new Set();
+
+                                        $.each(suggestions, function(index, customer) {
+                                            if (!seenIds.has(customer.user_id)) {
+                                                seenIds.add(customer.user_id);
+                                                uniqueSuggestions.push(customer);
+                                            }
+                                        });
+                                        $.each(uniqueSuggestions, function(index, customer) {
+                                            var item = $('<a>')
+                                                .addClass('list-group-item list-group-item-action')
+                                                .attr('href', "{{ route('customer.tree-view', ':id') }}".replace(':id', customer.user_id))
+                                                .html(customer.name + ' ( ' + customer.user_id + ')')
+                                                .on('click', function() {
+                                                    $('#search-query').val(customer.user_id); // Set input to customer name
+                                                    $('#search-form').submit(); // Submit the form
+                                                });
+                                            suggestionList.append(item);
+                                        });
+                                    } else {
+                                        // suggestionList.hide(); // Hide suggestions if no results
+                                        suggestionList.show(); // Show the suggestion list even if empty
+                                        suggestionList.append('<a class="list-group-item">No results found</a>');
+                                    }
+                                },
+                                error: function() {
+                                    // In case of an error in the AJAX request, hide suggestions
+                                    suggestionList.hide();
+                                }
+                            });
+                        } else {
+                            // $('#suggestions').hide(); // Hide suggestions when query length is less than 3
+                            suggestionList.show();
+                            suggestionList.append('<a class="list-group-item">Please enter at least 3 characters to search</a>');
+                        }
+                    });
+                });
             </script>
         @endsection
 
